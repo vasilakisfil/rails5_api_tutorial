d993bfe91b17e9231bb418edcee06678cdfcfd00
Michael Hartl <michael@michaelhartl.com>
2016-05-17 19:53:33 +0000

    Complete advanced setup

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ end
 
 group :test do
   gem 'minitest-reporters', '1.1.9'
+  gem 'guard',              '2.13.0'
   gem 'guard-minitest',     '2.4.4'
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
