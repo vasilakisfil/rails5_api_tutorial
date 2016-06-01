e89da7f9ed2a6b3112d819c0422fe56d20760e72
Michael Hartl <michael@michaelhartl.com>
2016-06-01 23:27:02 +0000

    Fix the test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new' 
+  get  '/signup',  to: 'users#new'
+  resources :users
 end
