07f46b618779597ccf0b79747e8b84552350ff89
Michael Hartl <michael@michaelhartl.com>
2016-06-06 23:42:24 +0000

    Remove superfluous line

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
