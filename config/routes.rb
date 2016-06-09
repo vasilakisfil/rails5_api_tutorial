bea76f51a14153461c2197c6d15f5d0379119b1e
Michael Hartl <michael@michaelhartl.com>
2016-06-09 22:22:38 +0000

    Uncomment micropost route

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts#,          only: [:create, :destroy]
+  resources :microposts,          only: [:create, :destroy]
 end
