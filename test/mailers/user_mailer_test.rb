8d40625c2afc9bca979e1b1e5d79562d493ce63e
Michael Hartl <michael@michaelhartl.com>
2016-06-09 22:17:38 -0700

    Switch to CGI.escape

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -11,7 +11,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal ["noreply@example.com"], mail.from
     assert_match user.name,               mail.body.encoded
     assert_match user.activation_token,   mail.body.encoded
-    assert_match CGI::escape(user.email), mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
   end
 
   test "password_reset" do
