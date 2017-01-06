5ac0508ab92fecdcc44e3f8e586c3d53bbb0bfe1
Filippos Vasilakis <vasilakisfil@gmail.com>
2017-01-06 16:29:00 +0100

    Add activation routes/controllers/ajax
    wip
    
    wip
    
    wip

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ApplicationMailer
+  helper_method :ember_activation_url
 
   def account_activation(user)
+    @user = user
+    mail to: user.email, subject: "Account activation"
+  end
+
+  def ember_account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
   end
@@ -8,5 +14,9 @@ class UserMailer < ApplicationMailer
   def password_reset(user)
     @user = user
     mail to: user.email, subject: "Password reset"
+  end
+
+  def ember_activation_url(token, email)
+    "#{Rails.application.secrets.ember_activation_url}?token=#{token}&email=#{email}".html_safe
   end
 end
